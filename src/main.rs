1c0f390dd77c122232034a3aa830a325208ef6be
Arktikus <ArktikusArkan@gmail.com>
2023-06-22 04:33:30 +0200

    Added colored text

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use rand::prelude::*;
 use std::{io, process::exit};
+use colored::Colorize;
 
 fn main() {
     clear_console();
 
     let mut guess = String::new();
 
-    println!("Guess a number between 1-10");
+    println!("Guess a number between: {}", "1-10".yellow());
     io::stdin().read_line(&mut guess).expect("Error");
 
     if guess.trim().is_empty() {
@@ -25,16 +26,16 @@ fn generate_random_number(guess: String) {
 
     let guess: u8 = guess.trim().parse().unwrap();
     if guess == x {
-        println!("Your guess: {} was correct!\n", guess);
+        println!("Your guess: {} was {}\n", guess, "correct!".green());
     } else {
-        println!("Your guess: {} was incorrect! \nIt was: {}\n", guess, x);
+        println!("Your guess: {} was {}\nIt was: {}\n", guess, "incorrect!".red(), x);
     }
     exit_or_not();
 }
 
 fn exit_or_not() {
     println!("Do you want to play a new round or exit?");
-    println!("For a new round type '1', to exit type '2'");
+    println!("For a new round type '{}', to exit type '{}'", "1".yellow(), "2".yellow());
 
     let mut answer = String::new();
     io::stdin().read_line(&mut answer).expect("Error");
@@ -44,7 +45,7 @@ fn exit_or_not() {
         //TODO - Add check what type answer is. If not u8 then go back to exit_or_not;
         
         clear_console();
-        println!("You need to provide an answer!\n");
+        println!("{}\n", "You need to provide an answer!".red());
         exit_or_not();
     }
 
@@ -57,7 +58,7 @@ fn exit_or_not() {
         exit(0)
     } else {
         clear_console();
-        println!("Not a valid option!\n");
+        println!("{}\n", "Not a valid option!".red());
         exit_or_not();
     }
 }
